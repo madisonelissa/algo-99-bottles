39e2207e27da8b0f271c3f4413bc45b4325866d7
madisonelissa <madisonefreese@gmail.com>
2023-05-23 20:39:34 -0700

    99 bottles problem

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,5 +1,17 @@
 function bottleSong() {
-  // Write your code here!
+  const prompt= require('prompt-sync')();
+  let inp = prompt('Enter bottles: ');
+  let bottles = Number(inp)
+  while (bottles > 2) {
+      console.log(`${bottles} bottles of beer on the wall, ${bottles} bottles of beer.\n
+      Take one down and pass it around, ${bottles-1} bottles of beer on the wall.`);
+      bottles = bottles - 1
+    };
+  console.log(`Take one down and pass it around, 1 bottle of beer on the wall.\n
+  1 bottle of beer on the wall, 1 bottle of beer.
+  Take one down and pass it around, no more bottles of beer on the wall.\n
+  No more bottles of beer on the wall, no more bottles of beer.
+  Go to the store and buy some more, 99 bottles of beer on the wall.`);
 };
 
 bottleSong();
